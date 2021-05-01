6b537414c723d23f5815557fab0b2edabfb51cb5
Dauryl <dauryl.belle@gmail.com>
2021-04-30 22:42:08 -0400

    Initial commit

---
+++ getfile_FIOS.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from getpass import getpass
+##Logging in to Verizon FIOS
+username = "dauryl.belle"
+password = "Xwn0g^7fY&Yc5"
+#password = getpass("Enter password: ")
+
+
+driver = webdriver.Chrome("/Users/dauryl/Documents/dbelle_scripts/get_FIOS_bill/chromedriver")
+driver.get("https://secure.verizon.com/vzauth/UI/Login")
+
+driver.implicitly_wait(5)
+
+username_textbox = driver.find_element_by_id("IDToken1")
+username_textbox.send_keys(username)
+
+password_textbox = driver.find_element_by_id("IDToken2")
+password_textbox.send_keys(password)
+
+login_button = driver.find_element_by_id("login-submit")
+login_button.submit()
+
+view_bill = driver.find_element_by_partial_link_text("View Bill Detail")
+view_bill.click()
+
+download = driver.find_element_by_partial_link_text("Download PDF")
+download.click()
+
