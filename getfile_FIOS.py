c593d9bf1088f904a5064fe6b37a6ec575935257
Ryl Belle <rylbelle>
2021-06-11 19:58:37 -0400

    FULLY WORKING SCRIPT with mail send.

--- getfile_FIOS.py
+++ getfile_FIOS.py
@@ -5,44 +5,42 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import time
 
-##Logging in to Verizon FIOS
-# username = input("Enter user: ")
-# password = getpass("Enter password: ")
-#
-#
-# driver = webdriver.Chrome("/Users/dbelle/Documents/dbelle_scripts/get_FIOS_bill/chromedriver")
-# driver.get("https://secure.verizon.com/vzauth/UI/Login")
-#
-# driver.implicitly_wait(5)
-#
-# username_textbox = driver.find_element_by_id("IDToken1")
-# username_textbox.send_keys(username)
-#
-# password_textbox = driver.find_element_by_id("IDToken2")
-# password_textbox.send_keys(password)
-#
-# login_button = driver.find_element_by_id("login-submit")
-# login_button.submit()
-#
-# view_bill = driver.find_element_by_partial_link_text("View Bill Detail")
-# view_bill.click()
-#
-# download = driver.find_element_by_partial_link_text("Download PDF")
-# download.click()
+#Logging in to Verizon FIOS
+username = input("Enter FIOS user: ")
+password = getpass("Enter FIOS password: ")
+password_email = getpass("Enter gmail password: ")
 
 
+driver = webdriver.Chrome("/Users/dbelle/Documents/dbelle_scripts/get_FIOS_bill/chromedriver")
+driver.get("https://secure.verizon.com/vzauth/UI/Login")
 
+driver.implicitly_wait(5)
 
+username_textbox = driver.find_element_by_id("IDToken1")
+username_textbox.send_keys(username)
 
-# password = getpass("Type your password and press enter: ")
+password_textbox = driver.find_element_by_id("IDToken2")
+password_textbox.send_keys(password)
+
+login_button = driver.find_element_by_id("login-submit")
+login_button.submit()
+
+view_bill = driver.find_element_by_partial_link_text("View Bill Detail")
+view_bill.click()
+
+download = driver.find_element_by_partial_link_text("Download PDF")
+download.click()
+
+time.sleep(5) # Sleep for 3 seconds
 
 # Create a secure SSL context
 subject = "An email with attachment from Python"
 body = "This is an email with attachment sent from Python"
 sender_email = "tt5775030@gmail.com"
 receiver_email = "dauryl.belle@gmail.com"
-password = getpass("Type your password and press enter:")
+
 
 # Create a multipart message and set headers
 message = MIMEMultipart()
@@ -54,7 +52,7 @@ message["Bcc"] = receiver_email  # Recommended for mass emails
 # Add body to email
 message.attach(MIMEText(body, "plain"))
 
-filename = "testfile.txt"  # In same directory as script
+filename = "/Users/dbelle/Downloads/paper-bill.pdf"  # In same directory as script
 
 # Open PDF file in binary mode
 with open(filename, "rb") as attachment:
@@ -79,5 +77,5 @@ text = message.as_string()
 # Log in to server using secure context and send email
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(sender_email, password)
+    server.login(sender_email, password_email)
     server.sendmail(sender_email, receiver_email, text)
