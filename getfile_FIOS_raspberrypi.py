0dcd73c1ba8da977b0393a202d0db2d9b40b4051
Dauryl <dauryl.belle@gmail.com>
2022-01-17 01:37:53 -0500

    Redo raspberry pi config

--- getfile_FIOS_raspberrypi.py
+++ getfile_FIOS_raspberrypi.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-#from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from getpass import getpass
 import smtplib,email,email.encoders,email.mime.text,email.mime.base
 #import smtplib, ssl
@@ -12,22 +12,23 @@ import time
 import os
 
 #Delete file if it already exists
-try:
-    os.remove('/home/dbelle')
-except OSError:
-    pass
+#try:
+#    os.remove('/home/dbelle')
+#except OSError:
+#    pass
 
 #Auth using env variables
 username = os.environ.get('USERNAME')
 password = os.environ.get('PASSWORD')
 password_email = os.environ.get('PASSWORD_EMAIL')
 
-display = Display(visible=0, size=(1600, 1200))
-display.start()
-driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
 
-#driver = webdriver.Chrome("/home/dbelle/scripts/get_FIOS_bill/chromedriver_pi")
-driver.get("https://secure.verizon.com/vzauth/UI/Login")
+chrome_options = Options()
+#chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver',chrome_options=chrome_options)
+driver.get('https://secure.verizon.com/vzauth/UI/Login')
 
 driver.implicitly_wait(5)
 
@@ -41,13 +42,13 @@ login_button = driver.find_element_by_id("login-submit")
 login_button.submit()
 
 #Challenge Question
-# secret_question = driver.find_element_by_id("IDToken1")
-# secret_question.send_keys("Trinidad")
-#
-# continue_button = driver.find_element_by_id("otherButton")
-# continue_button.submit()
+secret_question = driver.find_element_by_id("IDToken1")
+secret_question.send_keys("Trinidad")
 
-view_bill = driver.find_element_by_partial_link_text("View Bill Detail")
+continue_button = driver.find_element_by_id("otherButton")
+continue_button.submit()
+
+view_bill = driver.find_element_by_link_text("View Bill Details")
 view_bill.click()
 
 download = driver.find_element_by_partial_link_text("Download PDF")
@@ -56,47 +57,3 @@ download.click()
 time.sleep(5) # Sleep for 3 seconds
 
 driver.close()
-
-# Create a secure SSL context
-subject = "An email with attachment from Python"
-body = "This is an email with attachment sent from Python"
-sender_email = "tt5775030@gmail.com"
-receiver_email = "dbelle@salesforce.com"
-
-# Create a multipart message and set headers
-message = MIMEMultipart()
-message["From"] = sender_email
-message["To"] = receiver_email
-message["Subject"] = subject
-message["Bcc"] = receiver_email  # Recommended for mass emails
-
-# Add body to email
-message.attach(MIMEText(body, "plain"))
-
-filename = "/Users/dbelle/Downloads/paper-bill.pdf"  # In same directory as script
-
-# Open PDF file in binary mode
-with open(filename, "rb") as attachment:
-    # Add file as application/octet-stream
-    # Email client can usually download this automatically as attachment
-    part = MIMEBase("application", "octet-stream")
-    part.set_payload(attachment.read())
-
-# Encode file in ASCII characters to send by email
-encoders.encode_base64(part)
-
-# Add header as key/value pair to attachment part
-part.add_header(
-    "Content-Disposition",
-    f"attachment; filename= {filename}",
-)
-
-# Add attachment to message and convert message to string
-message.attach(part)
-text = message.as_string()
-
-# Log in to server using secure context and send email
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(sender_email, password_email)
-    server.sendmail(sender_email, receiver_email, text)
