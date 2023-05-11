725b73f20308805d7946ed10a4d21ba376de3866
tower01 <dauryl.belle@gmail.com>
2023-05-10 20:57:48 -0400

    THE SCRIPT WORKS FOR LINUX AND MAC. This copy is the working copy

--- getfile_FIOS.py
+++ getfile_FIOS.py
@@ -1,7 +1,9 @@
 import os
+import time
 import platform
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 from getpass import getpass
 import smtplib
 import ssl
@@ -9,7 +11,11 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 # Detect the current platform
 current_platform = platform.system()
@@ -23,19 +29,25 @@ if current_platform == 'Linux':
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
+# Set download directory depending on the platform
+download_directory = "/home/dbelle/Downloads" if current_platform == 'Linux' else "/Users/dbelle/Downloads"
+
+prefs = {"download.default_directory" : download_directory}
+options.add_experimental_option("prefs",prefs)
+
 chromedriver_path = '/usr/local/bin/chromedriver' if current_platform == 'Linux' else '/Users/dbelle/Documents/dbelle_scripts/get_FIOS_bill/chromedriver'
 
 # Initialize the webdriver with the new syntax
-driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
+#driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
+s=Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=s, options=options)
 
 # Delete file if it already exists
-pdf_path = "/home/dbelle/Downloads/paper-bill.pdf" if current_platform == 'Darwin' else "/path/to/your/ubuntu/pdf/location/paper-bill.pdf"
-
+pdf_path = "/home/dbelle/Downloads/paper-bill.pdf" if current_platform == 'Linux' else "/Users/dbelle/Downloads/paper-bill.pdf"
 try:
     os.remove(pdf_path)
 except OSError:
     pass
-
 
 # Auth using env variables
 username = os.environ.get('USERNAME')
@@ -50,13 +62,13 @@ driver.get("https://secure.verizon.com/vzauth/UI/Login")
 
 driver.implicitly_wait(5)
 
-username_textbox = driver.find_element_by_id("IDToken1")
+username_textbox = driver.find_element(By.ID, "IDToken1")
 username_textbox.send_keys(username)
 
-password_textbox = driver.find_element_by_id("IDToken2")
+password_textbox = driver.find_element(By.ID, "IDToken2")
 password_textbox.send_keys(password)
 
-login_button = driver.find_element_by_id("login-submit")
+login_button = driver.find_element(By.ID, "login-submit")
 login_button.submit()
 
 # Challenge Question
@@ -64,25 +76,32 @@ login_button.submit()
 
 def check_exists_by_xpath(xpath):
     try:
-        driver.find_element_by_xpath(xpath)
+        driver.find_element(By.XPATH, xpath)
     except NoSuchElementException:
         return False
     return True
 
 
 if check_exists_by_xpath("//input[@id='IDToken1']"):
-    secret_question = driver.find_element_by_id("IDToken1")
+    secret_question = driver.find_element(By.ID, "IDToken1")
     secret_question.send_keys(secret_question_answer)
-    continue_button = driver.find_element_by_id("otherButton")
+    continue_button = driver.find_element(By.ID, "otherButton")
     continue_button.submit()
 
 
 driver.implicitly_wait(20)
 
-view_bill = driver.find_element_by_partial_link_text("View Bill Detail")
+view_bill = driver.find_element(By.PARTIAL_LINK_TEXT, "View Bill Detail")
 view_bill.click()
 
-download = driver.find_element_by_partial_link_text("Download PDF")
+# Wait for the download link to become clickable
+wait = WebDriverWait(driver, 10)
+download_link_text = "Download PDF"  # adjust as needed
+download = wait.until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, download_link_text)))
+
+# Scroll the download link into view and click it
+driver.execute_script("arguments[0].scrollIntoView();", download)
+time.sleep(1)  # Wait for 1 second
 download.click()
 
 time.sleep(5)  # Sleep for 3 seconds
@@ -102,7 +121,8 @@ message["Subject"] = subject
 # Add body to email
 message.attach(MIMEText(body, "plain"))
 
-filename = "/Users/dbelle/Downloads/paper-bill.pdf"  # In same directory as script
+#filename = "/Users/dbelle/Downloads/paper-bill.pdf"  # In same directory as script
+filename = "/home/dbelle/Downloads/paper-bill.pdf" if current_platform == 'Linux' else "/Users/dbelle/Downloads/paper-bill.pdf"
 
 # Open PDF file in binary mode
 with open(filename, "rb") as attachment:
